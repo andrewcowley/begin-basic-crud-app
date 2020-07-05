b44fc86b6d10cb2b8c644e547f266b56cb3a518c
Andrew Cowley <andrew@andrewcowley.net>
2020-07-05 16:20:29 +1000

    try and make new endpoint

---
+++ src/http/post-inc/index.js
@@ -0,0 +1,12 @@
+let arc = require('@architect/functions')
+let data = require('@begin/data')
+
+exports.handler = async function post (req) {
+  await data.set({
+    table: 'inc',
+    value: 0
+  })
+  return {
+    statusCode: 200
+  }
+}
