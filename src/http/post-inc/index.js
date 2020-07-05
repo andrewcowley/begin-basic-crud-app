f49b27e6bb9863b8fbf775f038e40e30c1c2d2dd
Andrew Cowley <andrew@andrewcowley.net>
2020-07-05 16:26:10 +1000

    try and make new endpoint

--- src/http/post-inc/index.js
+++ src/http/post-inc/index.js
@@ -4,7 +4,9 @@ let data = require('@begin/data')
 exports.handler = async function post (req) {
   await data.set({
     table: 'inc',
-    value: 0
+    value: 0,
+    nine: 9,
+    aString: 'Great beach!'
   })
   return {
     statusCode: 200
